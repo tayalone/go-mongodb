05a450b9354cc2f9a19a81b4bcbcba565f88675a
panupong rong-o <tayalone@gmail.com>
2023-01-03 22:00:24 +0700

    :construction: ย้ายเครื่อง

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	MongoClient "github.com/tayalone/go-mongodb/mongo"
 )
@@ -23,29 +30,50 @@ func main() {
 	if errMongoClient != nil {
 		log.Fatalln("errMongoClient", errMongoClient.Error())
 	}
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:    ":8081", // listen and serve on 0.0.0.0:8081 (for windows "localhost:8080")
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+
+	// // create channel of os signal for waiting signal
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	// // close `quit` channel when app're closed
+	// defer func() {
+	// 	close(quit)
+	// }()
+	s := <-quit
+	log.Println("signal is: ", s)
+	log.Println("Shutting down app...")
+
+	// // The context is used to inform the server it has 5 seconds to finish
+	// // the request it is currently handling
+	// ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// defer cancel()
+
+	// // create context -> waiting every process in server is done
+	ctx := context.Background()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("App forced to shutdown:", err)
+	}
 	client.Deconnect()
-
-	// mongoUser := os.Getenv("MONGO_USER")
-	// if mongoUser == "" {
-	// 	log.Fatalln("MONGO_USER unset")
-	// }
-
-	// mongoPassword := os.Getenv("MONGO_PASSWORD")
-	// if mongoPassword == "" {
-	// 	log.Fatalln("MONGO_PASSWORD unset")
-	// }
-
-	// mongoEndpoint := os.Getenv("MONGO_ENDPOINT")
-	// if mongoEndpoint == "" {
-	// 	log.Fatalln("MONGO_ENDPOINT unset")
-	// }
-
-	// uri := fmt.Sprintf("mongodb+srv://%s:%s@%s?retryWrites=true&w=majority", mongoUser, mongoPassword, mongoEndpoint)
-
-	// _, errMgConnect := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-
-	// if errMgConnect != nil {
-	// 	log.Fatalln(errMgConnect.Error())
-	// }
 
 }
