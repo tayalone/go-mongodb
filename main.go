d71f612bfcba7189ee8a35eacc9f470488279de4
panupong rong-o <tayalone@gmail.com>
2023-01-03 10:51:57 +0700

    :alembic: try to connect db

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
 	fmt.Println("Let's Try")
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoUser := os.Getenv("MONGO_USER")
+	if mongoUser == "" {
+		log.Fatalln("MONGO_USER unset")
+	}
+
+	mongoPassword := os.Getenv("MONGO_PASSWORD")
+	if mongoPassword == "" {
+		log.Fatalln("MONGO_PASSWORD unset")
+	}
+
+	mongoEndpoint := os.Getenv("MONGO_ENDPOINT")
+	if mongoEndpoint == "" {
+		log.Fatalln("MONGO_ENDPOINT unset")
+	}
+
+	uri := fmt.Sprintf("mongodb+srv://%s:%s@%s?retryWrites=true&w=majority", mongoUser, mongoPassword, mongoEndpoint)
+
+	_, errMgConnect := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if errMgConnect != nil {
+		log.Fatalln(errMgConnect.Error())
+	}
+
 }
